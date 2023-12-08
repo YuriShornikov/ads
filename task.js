b434ccfb9056ff0f441a9537a555762ae869d684
Yuri Shornikov <dbonlyfun@gmail.com>
2023-12-08 09:28:59 +0300

    correct

--- task.js
+++ task.js
@@ -1,17 +1,17 @@
 const rotator__case = document.querySelectorAll('.rotator__case');
-let index = 0
+let index = 0;
 
 const func = function() {
-        const time = parseInt(rotator__case[index].getAttribute('data-speed'))
-        console.log(time)
-        rotator__case[index].classList.remove('rotator__case_active')
-        index = (index + 1) % rotator__case.length
-        rotator__case[index].classList.add('rotator__case_active')
-        const color = rotator__case[index].getAttribute('data-color')
-        rotator__case[index].style.color = color
+    // находим время, но не понятно, как его передать в setInterval 
+    const time = parseInt(rotator__case[index].getAttribute('data-speed'));
+    rotator__case[index].classList.remove('rotator__case_active');
+    index = (index + 1) % rotator__case.length;
+    rotator__case[index].classList.add('rotator__case_active');
+    const color = rotator__case[index].getAttribute('data-color');
+    rotator__case[index].style.color = color;
 
 }
 
-setInterval(func, 1000)
+setInterval(func, 1000);
 
 
