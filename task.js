b7d3fc3b9121aaad340af06db8242732e0471208
Yuri Shornikov <dbonlyfun@gmail.com>
2023-12-11 22:38:25 +0300

    all

--- task.js
+++ task.js
@@ -1,17 +1,23 @@
 const rotator__case = document.querySelectorAll('.rotator__case');
 let index = 0;
 
-const func = function() {
+
+
+
+
+
+const func = function(index) {
     // находим время, но не понятно, как его передать в setInterval 
-    const time = parseInt(rotator__case[index].getAttribute('data-speed'));
+    let time = parseInt(rotator__case[index].getAttribute('data-speed'));
     rotator__case[index].classList.remove('rotator__case_active');
     index = (index + 1) % rotator__case.length;
     rotator__case[index].classList.add('rotator__case_active');
     const color = rotator__case[index].getAttribute('data-color');
     rotator__case[index].style.color = color;
-
+    setTimeout(() => {func(index)}, time);
 }
 
-setInterval(func, 1000);
+func(index)
+
 
 
